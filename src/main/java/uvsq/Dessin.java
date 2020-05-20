4003f4c7d91a900142f9ea65928892b0fa60cc20
Edouard Longueville <edouard.longueville@ens.uvsq.fr>
2020-05-20 13:51:23 +0200

    Correction dessin

--- src/main/java/uvsq/Dessin.java
+++ src/main/java/uvsq/Dessin.java
@@ -1,4 +1,38 @@
 package uvsq;
 
-public class Dessin {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dessin implements Forme {
+
+    private List<Forme> childforms = new ArrayList<Forme>();
+
+    @Override
+    public void move(String coord){
+        for (Forme forme : childforms){
+            forme.move(coord);
+        }
+    }
+
+    @Override
+    public String show(){
+
+        String s;
+        s="";
+
+        for (Forme forme : childforms){
+            s=s+forme.show()+System.lineSeparator();
+        }
+
+        return s;
+    }
+
+    public void add(Forme f){
+        childforms.add(f);
+    }
+
+    public void remove(Forme f){
+        childforms.remove(f);
+    }
+
 }
